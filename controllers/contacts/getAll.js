cc2da69422bd9dde0f8fa9b737ff48317901c76c
Yana <yana2393@gmail.com>
2023-08-04 16:51:06 +0300

    steps 1-5

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,7 +1,17 @@
 const {Contact} = require("../../models/contact")
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const filter = { owner };
+
+  if (favorite === "true") {
+    filter.favorite = true;
+  } else if (favorite === "false") {
+    filter.favorite = false;
+  }
+  const result = await Contact.find(filter, "", { skip, limit });
   res.status(200).json(result);
 };
 
